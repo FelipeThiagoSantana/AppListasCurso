85e77f2f20d3dc2d3eb410d727153650ff2dc10e
felsilvaGirolando <felsilva@girolando.com.br>
2024-11-18 17:23:06 -0300

    Creating class Pessoa and implementation onclick method to get data the screen

--- app/src/main/java/com/example/applistascurso/view/MainActivity.java
+++ app/src/main/java/com/example/applistascurso/view/MainActivity.java
@@ -1,26 +1,87 @@
 package com.example.applistascurso.view;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.example.applistascurso.R;
+import com.example.applistascurso.model.Pessoa;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText editPrimeiroNome;
+    EditText editSobrenome;
+    EditText editCursoDesejado;
+    EditText editTelefoneContato;
+
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Pessoa pessoa = new Pessoa();
+
+        editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
+        editSobrenome = findViewById(R.id.editSobrenome);
+        editCursoDesejado = findViewById(R.id.editCursoDesejado);
+        editTelefoneContato = findViewById(R.id.editTelefoneContato);
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+
+        editPrimeiroNome.setText(pessoa.getPrimeiroNome());
+        editSobrenome.setText(pessoa.getSobrenome());
+        editCursoDesejado.setText(pessoa.getCursoDesejado());
+        editTelefoneContato.setText(pessoa.getTelefoneContato());
+
+
+       btnLimpar.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               editPrimeiroNome.setText("");
+               editSobrenome.setText("");
+               editCursoDesejado.setText("");
+               editTelefoneContato.setText("");
+
+               Toast.makeText(MainActivity.this, "Campos limpos", Toast.LENGTH_LONG).show();
+           }
+       });
+
+       btnFinalizar.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Toast.makeText(MainActivity.this, "Finalizando aplicação", Toast.LENGTH_LONG).show();
+               finish();
+           }
+       });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
+                pessoa.setSobrenome(editSobrenome.getText().toString());
+                pessoa.setCursoDesejado(editCursoDesejado.getText().toString());
+                pessoa.setTelefoneContato(editTelefoneContato.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Dados do " + pessoa.getPrimeiroNome() + " salvos com sucesso!" , Toast.LENGTH_LONG).show();
+
+                Log.i("Pessoa", pessoa.getPrimeiroNome());
+            }
         });
+
     }
 }
