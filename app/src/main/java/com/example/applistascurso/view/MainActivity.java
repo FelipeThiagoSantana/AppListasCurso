130d9f3269300e5663b3755ebc22d0bcbe301793
felsilvaGirolando <felsilva@girolando.com.br>
2024-11-18 12:42:42 -0300

    Creating MVC architecture

--- app/src/main/java/com/example/applistascurso/view/MainActivity.java
+++ app/src/main/java/com/example/applistascurso/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.applistascurso;
+package com.example.applistascurso.view;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.applistascurso.R;
 
 public class MainActivity extends AppCompatActivity {
 
