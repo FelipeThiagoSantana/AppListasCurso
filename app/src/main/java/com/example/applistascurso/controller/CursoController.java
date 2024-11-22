b86e0c8fe20613706645e2a81fe3a98bce5b29d5
FelipeThiagoSantana <felsilva@girolando.com.br>
2024-11-22 12:47:31 -0300

    implamentation classe CursoController and Curso in project

--- app/src/main/java/com/example/applistascurso/controller/CursoController.java
+++ app/src/main/java/com/example/applistascurso/controller/CursoController.java
@@ -1,4 +1,23 @@
 package com.example.applistascurso.controller;
 
+import com.example.applistascurso.model.Curso;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CursoController {
+
+    public List listCursos;
+
+    public List getListaCursos() {
+
+        listCursos = new ArrayList<Curso>();
+
+        listCursos.add(new Curso());
+        listCursos.add(new Curso());
+        listCursos.add(new Curso());
+        listCursos.add(new Curso());
+
+        return listCursos;
+    }
 }
--- app/src/main/java/com/example/applistascurso/model/Curso.java
+++ app/src/main/java/com/example/applistascurso/model/Curso.java
@@ -1,4 +1,13 @@
 package com.example.applistascurso.model;
 
 public class Curso {
+    private String nomeDoCursoDesejado;
+
+    public String getNomeDoCursoDesejado() {
+        return nomeDoCursoDesejado;
+    }
+
+    public void setNomeDoCursoDesejado(String nomeDoCursoDesejado) {
+        this.nomeDoCursoDesejado = nomeDoCursoDesejado;
+    }
 }
