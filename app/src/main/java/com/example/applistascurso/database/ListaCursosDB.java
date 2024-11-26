64ea352d81ae83af41d38b2294d3269bf75a2bb4
FelipeThiagoSantana <felipesantana27@gmail.com>
2024-11-26 16:31:25 -0300

    Create class to database

---
+++ app/src/main/java/com/example/applistascurso/database/ListaCursosDB.java
@@ -0,0 +1,45 @@
+package com.example.applistascurso.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.applistascurso.controller.CursoController;
+
+public class ListaCursosDB extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "Pessoa.db";
+    public static final int DB_VERSION = 1;
+
+    Cursor cursor;
+
+    SQLiteDatabase db;
+
+    public ListaCursosDB(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+
+        db = getWritableDatabase();
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String sqlTabela = "CREATE TABLE IF NOT EXISTS pessoas(id INTEGER PRIMARY KEY AUTOINCREMENT,)"+
+                "nome TEXT,"+
+                "sobrenome TEXT,"+
+                "cursoDesejado TEXT,"+
+                "telefoneContato TEXT" +
+                "cursoDesejado TEXT"+
+                ")";
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
